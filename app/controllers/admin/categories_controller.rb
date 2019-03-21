8df562594e5e48e48b2998857d18446bbea171df
GideonBrasil <gidxx_z@yahoo.com>
2019-03-21 05:39:21 +0000

    finish views, fix controller bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,8 +20,8 @@ class Admin::CategoriesController < ApplicationController
     private
 
     def category_params
-        params.require(:product).permit(
-        :name,
+        params.require(:category).permit(
+        :name
         )
     end
 end
