0870da0a185f9b52dae6c56b7537c1b6f5be00c6
GideonBrasil <gidxx_z@yahoo.com>
2019-03-22 16:25:07 +0000

    remove some mailer generate files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      # Tell the UserMailer to send a welcome email after save
+      UserMailer.welcome_email(@user).deliver_later
+ 
+      format.html { redirect_to(@user, notice: 'User was successfully created.') }
+      format.json { render json: @user, status: :created, location: @user }
+      #send user to root after loging in
       redirect_to :root
     else
       render :new
     end
+
   end
 
   private
