6c188569da5fc54b53f44b9ce33877f2cfd264ad
GideonBrasil <gidxx_z@yahoo.com>
2019-03-23 02:32:50 +0000

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,14 @@
 class Review < ActiveRecord::Base
 
     belongs_to :product
-    
+    belongs_to :user
+
+    validates :user_id, presence: { message: "Must sign in to review a product" }
+    validates :user_id, uniqueness: { scope: :product_id, message: "You can only rate a product once" }
+    validates :product_id, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true,
+              numericality: true,
+              inclusion: { in: 1..5 }
+
 end
