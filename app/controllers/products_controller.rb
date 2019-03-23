50bc7bcb1ca2f4dd8833e84d5bf3246b1074543c
GideonBrasil <gidxx_z@yahoo.com>
2019-03-23 21:52:32 +0000

    rating and rating average

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Review.new
+    @reviewAvg = Review.where(product_id: @product.id).average(:rating).to_i.round 1
+    @reviewPeople = Review.where(product_id: @product.id).sum(:rating).to_i
   end
 
 end
