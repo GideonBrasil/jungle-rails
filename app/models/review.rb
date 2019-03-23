35a74c13db7046112c8a5b40425644ad5b307f0e
GideonBrasil <gidxx_z@yahoo.com>
2019-03-23 13:21:00 +0000

    reviews displaying for products

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,11 @@
 class Review < ActiveRecord::Base
 
-    belongs_to :product
-    belongs_to :user
+    belongs_to :products
+    belongs_to :users
 
-    validates :user_id, presence: { message: "Must sign in to review a product" }
-    validates :user_id, uniqueness: { scope: :product_id, message: "You can only rate a product once" }
-    validates :product_id, presence: true
+    # validates :user_id, presence: { message: "Must sign in to review a product" }
+    # validates :user_id, uniqueness: { scope: :product_id, message: "You can only rate a product once" }
+    # validates :product_id, presence: true
     validates :description, presence: true
     validates :rating, presence: true,
               numericality: true,
