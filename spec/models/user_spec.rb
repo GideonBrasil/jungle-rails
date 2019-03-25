b00532923ef1ef27fac18c2e38edf9e540b234a0
GideonBrasil <gidxx_z@yahoo.com>
2019-03-25 23:45:10 +0000

    first validation for saving user in the db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  subject { 
+    described_class.new(password: 'wofwof',
+                           email: 'john@doe.com',
+                           first_name: 'Mazli',
+                                last_name: 'Barton',
+                                password_confirmation: 'wofwof')
+           }
+
   
   describe 'Validations' do
 
-    it 'will only save to db if user contains password, password_confirmation, email, first_name and last_name'
+    it 'will only save to db if user contains password, password_confirmation, email, first_name and last_name' do
+      expect(subject).to be_truthy
+    end
+
     it 'is not valid without a first_name'
     it 'is not valid without a last_name'
     it 'is not valid without an email'
