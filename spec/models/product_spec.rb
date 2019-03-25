fde533c1bf9a1ae9ceb076a52b33a400e3b7f6e6
GideonBrasil <gidxx_z@yahoo.com>
2019-03-25 23:08:21 +0000

    must have price validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,7 +20,13 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include "Name can't be blank"
     end
 
-    it 'is not valid without a price'
+    it 'is not valid without a price' do
+      @product.price_cents = nil
+      @product.save
+      # expect(@product.errors.messages).to include name: ["can't be blank"] 
+      expect(@product.errors.full_messages).to include "Price can't be blank"
+    end
+
     it 'is not valid without a quantity'
     it 'is not valid without a category_id'
   end
