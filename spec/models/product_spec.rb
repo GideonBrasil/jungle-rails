83c8457b34ef1497b9414ac54d89c7cebb275bdf
GideonBrasil <gidxx_z@yahoo.com>
2019-03-25 22:45:27 +0000

    validate save product to db

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  before do
+    @category = Category.new(name: 'Automobiles')
+    @product = Product.new(name: 'Civic', price: 5, quantity: 5, category: @category)
+  end
+
   describe 'Validations' do
-    # test code here
+    it 'will save successfully to db if product contains name, price, quantity and a category' do
+     expect(@product.save).to be_truthy
+    end
+
+
+    it 'is not valid without a name' 
+
+    it 'is not valid without a price'
+    it 'is not valid without a quantity'
+    it 'is not valid without a category_id'
   end
 
 end
