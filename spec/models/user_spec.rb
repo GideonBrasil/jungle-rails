a2d12e076631e705bcc49587706709c39f1117c6
GideonBrasil <gidxx_z@yahoo.com>
2019-03-25 23:29:44 +0000

    write out validation test casses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+
+    it 'will only save to db if user contains password, password_confirmation, email, first_name and last_name'
+    it 'is not valid without a first_name'
+    it 'is not valid without a last_name'
+    it 'is not valid without an email'
+    it 'is not valid without a password'
+    it 'is not valid without a password_confirmation'
+
+  end
+
 end
