16baf012f42e925b14777a2acd14f1c1cc6fe7db
GideonBrasil <gidxx_z@yahoo.com>
2019-03-26 21:47:45 +0000

    complete user login test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature "UserLogins", type: :feature, js: true do
+RSpec.feature "Determine that a user is logged in", type: :feature, js: true do
   # SETUP
 before :each do
 
   @user = User.create!(
-    name: 'Mazli',
-    email: 'mazli@gamil.com',
+    first_name: 'Mazli',
+    last_name: 'Barton',
+    email: 'mazli@gmail.com',
     password: 'wofwof',
     password_confirmation: 'wofwof'
   )
+
   @category = Category.create! name: 'Apparel'
   10.times do |n|
     @category.products.create!(
@@ -20,22 +22,28 @@ before :each do
       price: 64.99
     )
     end
-  
   end
 
-  scenario "They see that a product was added to cart" do
+  scenario "They see that their email is displayed on the navbar" do
     # ACT
-    visit root_path
+    visit new_session_path
+    
+    within 'form' do
+      fill_in id: 'session_email', with: 'mazli@gmail.com'
+      fill_in id: 'session_password', with: 'wofwof'
 
+      click_button 'Sign in'
+    end
+
+    visit root_path
     #code
-    first('.product').click_button('Add')
 
     # DEBUG / VERIFY
     # save_screenshot
     # Index page should have article.product
     # expect(page).to have_css 'article.product'
-    expect(page).to have_text 'My Cart (1)'
-    # save_screenshot
+    expect(page).to have_content 'Signed in as mazli@gmail.com'
+    save_screenshot
   end
   
 end
