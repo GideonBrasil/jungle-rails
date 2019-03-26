92f7f571b8037dadcd2ce0266c65cb5c2dfb6def
GideonBrasil <gidxx_z@yahoo.com>
2019-03-26 00:45:15 +0000

    won't save to db if trying to sign up with same email (not case sensitive)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+
   subject { 
     described_class.new(password: 'wofwof',
-                           email: 'john@doe.com',
+                           email: 'mazli@gmail.com',
                       first_name: 'Mazli',
                        last_name: 'Barton',
            password_confirmation: 'wofwof')
           }
-
-  # subject2 {
-  #   described_class.new(password: 'wofwof',
-  #                          email: 'mazli@gmail.com',
-  #                     first_name: 'Mazli',
-  #                      last_name: 'Barton',
-  #          password_confirmation: 'wofwofwof')
-  #           }
+  before do
+    @user2 = User.new(password: 'wofwof',
+                        email: 'MAZLI@gmail.com',
+                    first_name: 'Mazli',
+                    last_name: 'Barton',
+        password_confirmation: 'wofwof')
+  end
   
   describe 'Validations' do
 
@@ -27,6 +27,11 @@ RSpec.describe User, type: :model do
     it 'will not save to db if user password & password_confirmation aren"t the same' do
       subject.password_confirmation = 'wofwofwoff'
       expect(subject.save).to be_falsey
+    end
+
+    it 'will not save to db if user emails are already in the db not case_sesitive' do
+      subject.save
+      expect(@user2.save).to be_falsey
     end
 
     it 'is not valid without a first_name'
