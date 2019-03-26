7f701a663c1dd7021b9a4538de47aac7d70a3100
GideonBrasil <gidxx_z@yahoo.com>
2019-03-26 00:20:28 +0000

    test for password_confirmation !== password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,28 @@ RSpec.describe User, type: :model do
   subject { 
     described_class.new(password: 'wofwof',
                            email: 'john@doe.com',
-                           first_name: 'Mazli',
-                                last_name: 'Barton',
-                                password_confirmation: 'wofwof')
-           }
+                      first_name: 'Mazli',
+                       last_name: 'Barton',
+           password_confirmation: 'wofwof')
+          }
 
+  # subject2 {
+  #   described_class.new(password: 'wofwof',
+  #                          email: 'mazli@gmail.com',
+  #                     first_name: 'Mazli',
+  #                      last_name: 'Barton',
+  #          password_confirmation: 'wofwofwof')
+  #           }
   
   describe 'Validations' do
 
     it 'will only save to db if user contains password, password_confirmation, email, first_name and last_name' do
-      expect(subject).to be_truthy
+      expect(subject.save).to be_truthy
+    end
+
+    it 'will not save to db if user password & password_confirmation aren"t the same' do
+      subject.password_confirmation = 'wofwofwoff'
+      expect(subject.save).to be_falsey
     end
 
     it 'is not valid without a first_name'
